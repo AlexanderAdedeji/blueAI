885fa03512ab607733ae3fbdf8c14d3385e2b14f
AlexAdedeji <alexadedeji15@gmail.com>
2024-06-11 00:37:41 -0700

    added themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
- 
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "./providers/theme-provider";
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +28,14 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-  
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
