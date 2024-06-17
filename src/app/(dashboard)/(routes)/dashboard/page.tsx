e246f49d71faedf94086c5a300a816b58e5a5853
AlexAdedeji <alexadedeji15@gmail.com>
2024-06-17 17:38:20 +0100

    route group ad landing pahge

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { defaultConfig } from "next/dist/server/config-shared";
+import Image from "next/image";
+
+const DashboardPage =() => {
+  return <main>Hello WOlrd</main>;
+}
+
+
+export default DashboardPage
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+import { defaultConfig } from "next/dist/server/config-shared";
+import Image from "next/image";
+
+const LandingPage = () => {
+  return <main>Hello WOlrd </main>;
+};
+
+export default LandingPage;
